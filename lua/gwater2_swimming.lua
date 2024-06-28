46bd41241f386f7f4a3959f22d81531aeff127c3
Meetric <73554599+meetric1@users.noreply.github.com>
2024-06-27 21:41:27 -0400

    cancel fall damage in water

--- lua/gwater2_swimming.lua
+++ lua/gwater2_swimming.lua
@@ -1,20 +1,19 @@
 
-local GWATER2_PARTICLES_TO_SWIM = 30	-- TODO: Move to separate file
+local GWATER2_PARTICLES_TO_SWIM = 30
 
--- swimming code
+-- swim code provided by kodya (with permission)
 local gravity_convar = GetConVar("sv_gravity")
 local function in_water(ply) 
 	if ply:OnGround() then return false end
 	return ply.GWATER2_CONTACTS and ply.GWATER2_CONTACTS >= GWATER2_PARTICLES_TO_SWIM
 end
 
--- swim code provided by kodya
-hook.Add("CalcMainActivity", "gwater2_water_swimming", function(ply)
+hook.Add("CalcMainActivity", "gwater2_swimming", function(ply)
 	if !in_water(ply) or ply:InVehicle() then return end
 	return ACT_MP_SWIM, -1
 end)
 
-hook.Add("Move", "gwater2_water_swimming", function(ply, move)
+hook.Add("Move", "gwater2_swimming", function(ply, move)
 	if !in_water(ply) then return end
 
 	local vel = move:GetVelocity()
@@ -43,7 +42,7 @@ hook.Add("Move", "gwater2_water_swimming", function(ply, move)
 	move:SetVelocity(vel * 0.99)
 end)
 
-hook.Add("FinishMove", "gwater2_water_swimming", function(ply, move)
+hook.Add("FinishMove", "gwater2_swimming", function(ply, move)
 	if !in_water(ply) then return end
 	local vel = move:GetVelocity()
 	local pgrav = ply:GetGravity() == 0 and 1 or ply:GetGravity()
@@ -51,4 +50,9 @@ hook.Add("FinishMove", "gwater2_water_swimming", function(ply, move)
 
 	vel.z = vel.z + FrameTime() * gravity
 	move:SetVelocity(vel)
+end)
+
+-- cancel fall damage when in water
+hook.Add("GetFallDamage", "gwater2_swimming", function(ply, speed)
+	if ply.GWATER2_CONTACTS and ply.GWATER2_CONTACTS >= GWATER2_PARTICLES_TO_SWIM then return 0 end
 end)
